f1690a7fbdf566de2488174b5d797131286b47e6
_Slothy_ <ellis.morgan09@icloud.com>
2022-10-24 18:32:44 +0100

    Modules Set up

--- src/main/java/quineClient/mixins/MinecraftClientMixin.java
+++ src/main/java/quineClient/mixins/MinecraftClientMixin.java
@@ -1,5 +1,19 @@
 package quineClient.mixins;
 
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.minecraft.client.MinecraftClient;
+import quineClient.Client;
+
+
+
+@Mixin(MinecraftClient.class)
 public class MinecraftClientMixin {
 	
+	@Inject(method = "tick", at = @At("HEAD"), cancellable = true)
+	public void onTick(CallbackInfo ci) {
+		Client.INSTANCE.onTick();	}
 }
